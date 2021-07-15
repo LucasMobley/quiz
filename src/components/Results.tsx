b0d0291cf143420903258c0ec13893d1ff66c848
Lucas Mobley <Lucas@LucasMobley.com>
2021-07-15 08:07:27 -0700

    more documentation added

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,14 @@
 import React, {MouseEvent} from 'react';
 import { useHistory } from "react-router-dom";
 
+/*
+This page receives props from DataContainer. It has one button element that sends users back to the home page on click.
+I map over my questions array, using that index # to also access the answers array containing - and +.
+There are a fair number of class names used here (although still minimal) for neccessary styling to this page.
+
+
+*/
+
 type ResultProps = {
   questions: any[];
   answers: string[];
@@ -29,7 +37,7 @@ const Results: React.FC<ResultProps> = ({questions, answers, score}) => {
       <p>You scored</p>
       <h1>{score} / 10</h1>
       <p>{results}</p>
-      <button onClick={playAgain}>PLAY AGAIN?</button>
+      <button className='playagain' onClick={playAgain}>PLAY AGAIN?</button>
     </div>
   )
 }
